4710cac67776543a98a45988ec25c10140804260
iwawomaru <mosawa@ailab.ics.keio.ac.jp>
2016-07-26 09:51:36 +0900

    add ESN as a sample of Circuit and Planner.

---
+++ noh/components/esn.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from noh import Circuit, Planner
+from noh.components import Layer, Reservoir
+from noh.training_functions import gen_linear_regression_trainer
+# from noh.utils import sigmoid, p_sig, get_lr_func
+
+class ESN_Planner(Planner):
+    def __init__(self, components):
+        super(ESN_Planner, self).__init__(components)
+        print self.components
+
+    def __call__(self, data):
+        data = self.components[0].prop_up_sequence(data)
+        data = self.components[1](data)
+        return data
+
+    def train(self, data, label, epochs):
+        resv = self.components[0].prop_up_sequence(data)
+        self.components[1].train(resv, label, epochs=1)
+        
+
+class ESN(Circuit):
+    def __init__(self, n_visible, n_hidden, n_output):
+        super(ESN, self).__init__(components=None, planner=None)
+
+        resv = Reservoir(n_visible, n_hidden, bind_prob_W=0.03, bind_prob_W_rec=0.1)
+        liner_regression = Layer(n_hidden, n_output, train_func_generator=gen_linear_regression_trainer)
+        
+        self.components = (resv, liner_regression)
+        self.planner = ESN_Planner(self.components)
