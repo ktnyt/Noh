58d138a9a22043d1cd1be441a818e9186f79d262
iwawomaru <mosawa@ailab.ics.keio.ac.jp>
2016-08-03 17:59:25 +0900

    env __init__.py

--- noh/environments/__init__.py
+++ noh/environments/__init__.py
@@ -3,7 +3,11 @@ from noh.environments.simple_test_env import SimpleTest
 from noh.environments.simple_unsupervised_env import SimpleUnsupervisedTest
 from noh.environments.sin_to_cos_env import Sin2CosTest
 from noh.environments.mnist_env import MNISTEnv
+from noh.environments.behavioral_test_battery import BehavioralTestBattery 
 from noh.environments.eight_arm_radial_maze_env import EightArmRadialMaze
+
+from noh.environments.simple_pong_env import Pong
+from noh.environments.lunar_lander_v2_env import LunarLander_v2
 #from noh.environments.mnist_env import MovingBallEnv
 #from noh.environments.walking_nao_32_env import WalkingNao32Env
 #from noh.environments.mnist_env import WalkingNao64Env
