be2466002328c5c1aa76f063cfa8df184dbc5bcd
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-04-11 18:02:23 +0900

    Add specific constraints

--- noh/core.py
+++ noh/core.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-from inspect import signature, Signature, Parameter
+from abc import ABCMeta, abstractmethod
 
 
 def connections_to_edges(connections):
@@ -21,16 +21,42 @@ class Component(object):
     def __init__(self):
         self.buffer = {}
 
-    def get(self, name):
-        return self.buffer[name]
+    def get(self, key):
+        return self.buffer[key]
 
-    def set(self, name, value):
-        self.buffer[name] = value
+    def set(self, key, value):
+        self.buffer[key] = value
+
+
+class Sensor(Component):
+    def __init__(self):
+        super(Sensor, self).__init__()
+        self.buffer = None
+
+    def get(self, _):
+        return self.buffer
+
+    def set(self, _, value):
+        self.buffer = value
+
+    def input(self, value):
+        self.buffer = value
+
+
+class Actuator(Component, metaclass=ABCMeta):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        super(Actuator, self).__init__()
+
+    @abstractmethod
+    def output(self):
+        raise NotImplementedError()
 
 
 class Circuit(object):
-    def __init__(self, edges):
-        self.uuid = uuid4()
+    def __init__(self, name, edges):
+        self.name = name
         self.edges = edges
         self.funcs = [None] * len(edges)
 
@@ -38,12 +64,13 @@ class Circuit(object):
         if None in self.funcs:
             raise NotImplementedError()
 
-        for (pre, post), func in zip(self.edges, self.funcs):
-            pre.set(self.uuid, arg)
-            arg = func(arg)
-            post.set(self.uuid, arg)
+        if arg is not None:
+            self.edges[0][0].set(self.name, arg)
 
-        return arg
+        for (pre, post), func in zip(self.edges, self.funcs):
+            post.set(self.name, func(pre.get(self.name)))
+
+        return post.get(self.name)
 
     def implement(self, *funcs):
         if len(funcs) != len(self.funcs):
@@ -61,6 +88,9 @@ class Architecture(Component):
         self.nodes = components
         self.circuits = {}
 
+        self.inputs = list(self.nodes.keys())
+        self.outputs = list(self.nodes.keys())
+
         for key in self.edges:
             pre, post = key
 
@@ -70,12 +100,32 @@ class Architecture(Component):
             if post not in components:
                 raise Exception('no component with name {}'.format(post))
 
+            if pre in self.outputs:
+                self.outputs.remove(pre)
+
+            if post in self.inputs:
+                self.inputs.remove(post)
+
+        for key in self.inputs:
+            if not isinstance(self.nodes[key], Sensor):
+                raise Exception('input {} is not a Sensor'.format(key))
+
+        for key in self.outputs:
+            if not isinstance(self.nodes[key], Actuator):
+                raise Exception('output {} is not an Actuator'.format(key))
+
     def add_circuit(self, name, circuit):
         if name in self.circuits:
             raise Warning('a circuit with name {} already exists'.format(name))
         self.circuits[name] = circuit
 
     def create_circuit(self, name, keys):
+        if keys[0] not in self.inputs:
+            raise Exception('{} is not an input node'.format(keys[0]))
+
+        if keys[-1] not in self.outputs:
+            raise Exception('{} is not an output node'.format(keys[-1]))
+
         wanted = zip(keys[:-1], keys[1:])
         edges = []
 
@@ -85,9 +135,24 @@ class Architecture(Component):
             pre, post = edge
             edges.append((self.nodes[pre], self.nodes[post]))
 
-        circuit = Circuit(edges)
+        circuit = Circuit(name, edges)
         self.add_circuit(name, circuit)
         return circuit
 
     def run_circuit(self, name, arg):
         return self.circuits[name](arg)
+
+    def __call__(self, **args):
+        for key in self.inputs:
+            if key not in args:
+                raise Exception('required parameter: {}', key)
+
+        for key, value in args.items():
+            if key not in self.inputs:
+                raise Exception('invalid parameter: {}', key)
+            self.nodes[key].input(value)
+
+        for circuit in self.circuits.values():
+            circuit(None)
+
+        return {key: self.nodes[key].output() for key in self.outputs}
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
-
+import random
 from nose.tools import eq_
 
-from noh.core import Architecture, Circuit, Component
+from noh.core import Architecture, Circuit, Component, Sensor, Actuator
+
+
+class PFC(Actuator):
+    def __init__(self):
+        super(PFC, self).__init__()
+
+    def output(self):
+        keys =list(self.buffer.keys())
+        return self.buffer[random.choice(keys)]
 
 
 class SeedWBA(Architecture):
     def __init__(self):
         super(SeedWBA, self).__init__(
             dict(
-                sa=Component(),
+                sa=Sensor(),
                 hip=Component(),
-                bg=Component(),
-                pfc=Component(),
                 amg=Component(),
+                bg=Component(),
+                pfc=PFC(),
             ), [
                 (('hip', 'sa'), 'bg'),
                 (('amg', 'sa'), 'hip'),
-                (('hip', 'bg'), 'pfc'),
                 ('hip', 'amg'),
+                (('hip', 'bg'), 'pfc'),
             ])
 
 
@@ -29,3 +38,4 @@ def test_architecture():
     test.implement(double, double)
 
     eq_(seedwba.run_circuit('test', 42), 168)
+    eq_(seedwba(sa=42), {'pfc': 168})
